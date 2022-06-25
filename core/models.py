e5e5d3284c3d85cddf39543d69afa8d3c5fc8a17
Emmxnuel21 <101678442+Emmxnuel21@users.noreply.github.com>
2022-06-24 21:06:39 -0400

    SE AGREGA BASE DE DATOS Y SE CREA LA TABLA PARA EL REGISTRO DE USUARIO
    se agrega la base de datos locar y se crea la tabla para el formulario de registro de usuarios.

--- core/models.py
+++ core/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class registroUsuarios(models.Model):
+    nickname = models.CharField(max_length=16, verbose_name="usuario")
+    nombre = models.CharField(max_length=16, verbose_name="nombre del usuario")
+    apellidoPaterno = models.CharField(max_length=16, verbose_name="apellido paterno del ususario")
+    apellidoMaterno = models.CharField(max_length=16, verbose_name="apellido materno del usuario")
+    contrasena = models.CharField(max_length=12, verbose_name="contraseña del usuario")
+    correo = models.CharField(max_length=40, verbose_name="correo del usuario")
+    telefono = models.IntegerField(verbose_name="telefono del usuario")
+    nombreMascota = models.CharField(max_length=16, verbose_name="nombre de la mascota")
+    edadMascota = models.IntegerField(verbose_name="edad de la mascota")
+
+    def __str__(self):
+        return self.nickname
 
